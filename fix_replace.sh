0100bc12638c02f5a39e01bb1835dfab437e63ec
Lars Hvam <5888506+larshp@users.noreply.github.com>
2026-04-08 10:34:53 +0200

    hmm

---
+++ fix_replace.sh
@@ -0,0 +1,1 @@
+git checkout src/zcl_mjs.clas.abap
---
+++ test2.mjs
@@ -0,0 +1,3 @@
+import { Statements, String } from "@abaplint/runtime";
+import { ABAPRegExp } from "@abaplint/runtime";
+// wait I can't import ABAPRegExp directly without hacking
